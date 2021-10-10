5c8124e11e20daaa67e2277d1b5112e321b0471c
efraim9 <68015670+efraim9@users.noreply.github.com>
2021-10-11 00:48:11 +0200

    Examples for convertStories method options

--- puppeteer-convert-stories.js
+++ puppeteer-convert-stories.js
@@ -2,4 +2,7 @@ const convertStories = require('e2e-stories')
 
 convertStories({
   out: './test/spec', // path to JS test files output folder
+  outputType: "puppeteer", // Currently only Puppeteer is available, cypress and other tools will be added soon.
+  headers: {"bypass-Token": "SFSA-1625-REWQ-9531"}, // External HTTP headers to be sent with every request on the page
+  pathToYaml: "*(playground|test|tests|spec|src|build)/**/stories/**/*.+(yaml|yml)" // Glob pattern for finding yaml files, be careful with the configuration
 })
